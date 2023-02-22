368f934607407a99a83afb4c40fb673eea23a6da
zkcast2 <110002108+Zkcast@users.noreply.github.com>
2023-02-22 13:08:20 -0300

    navbar responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -9,6 +9,7 @@ export const NavBar = () => {
 
     const [scrolled, setScrolled] = useState(true);
 
+
     useEffect(() => {
         function handleScroll() {
             if (window.scrollY >= 200) {
@@ -23,32 +24,37 @@ export const NavBar = () => {
         return () => {
             window.removeEventListener('scroll', handleScroll);
         }
+        
     }, []);
+
 
     return (
         <React.Fragment>
             <div className={`${!scrolled && 'scrolled'} navbar_custom shadowbottom`}>
-                <nav className={"navbar bg-transparent navbar-expand-lg navbar-light bg-light"}>
+                <nav className={`navbar bg-transparent navbar-expand-lg navbar-light bg-light d-flex justify-content-end`}>
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <div className='d-flex justify-content-end'>
+                        <button className="navbar-toggler mx-3 navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
 
-                    <div className="collapse navbar-collapse px-3 mx-3 d-flex justify-content-end navbar-items" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="" onClick={() => setSelected('home')}>
+                    <div className={`${scrolled && 'collapse'} collapse navbar-collapse px-3 mx-3 text-end justify-content-end navbar-items`} id="navbarNav">
+                        <ul className="navbar-nav d-flex justify-content-end navbarItems">
+                            <li className="d-flex justify-content-end navItem" onClick={() => setSelected('home')}>
                                 <Link to='/' className="nav-link"><span className={selected === 'home' ? 'neon' : 'navitem'}>Home</span></Link>
                             </li>
-                            <li className="" onClick={() => setSelected('contact')}>
+                            <li className="d-flex justify-content-end navItem" onClick={() => setSelected('contact')}>
                                 <Link to='/contact' className="nav-link"><span className={selected === 'contact' ? 'neon' : 'navitem'}>Contacto</span></Link>
                             </li>
                             <Link to='/priceme' className="nav-link">
-                                <li className="" onClick={() => setSelected('price')}>
+                                <li className="d-flex justify-content-end navItem" onClick={() => setSelected('price')}>
                                     <span className={selected === 'price' ? 'neon' : 'navitem'}>Presupuesto</span>
                                 </li>
                             </Link>
                         </ul>
                     </div>
+
                 </nav>
             </div>
         </React.Fragment>
